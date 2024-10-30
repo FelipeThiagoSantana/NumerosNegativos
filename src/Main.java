2cde3f180193bc9022b3ca598670c535114ea0c1
FelipeThiagoSantana <107277318+FelipeThiagoSantana@users.noreply.github.com>
2024-10-29 23:11:16 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+
+import Entities.Number;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Quantos numeros voce vai digitar?");
+        int n = scanner.nextInt();
+
+        int[] vet = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.printf("Digite um numero: ");
+            vet[i] = scanner.nextInt();
+        }
+
+        System.out.println("Numeros negativos: ");
+        for (int i = 0; i < n; i++) {
+            if (vet[i] < 0) {
+                System.out.println(vet[i]);
+            }
+        }
+        scanner.close();
+    }
+}
